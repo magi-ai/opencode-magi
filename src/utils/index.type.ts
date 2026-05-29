17e4edf301f0442715187c8658ea2aa2638e1b1a
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2026-05-29 21:28:43 +0900

    feat(types): add Primitive and DeepPartial types for improved type handling

--- src/utils/index.type.ts
+++ src/utils/index.type.ts
@@ -1,3 +1,18 @@
+export type Primitive =
+  | bigint
+  | boolean
+  | null
+  | number
+  | string
+  | symbol
+  | undefined
+
 export interface Dict<Y = any> {
   [key: string]: Y
 }
+
+export type DeepPartial<Y> = Y extends any[] | Date | Function | Primitive
+  ? Y
+  : {
+      [P in keyof Y]?: DeepPartial<Y[P]>
+    }
