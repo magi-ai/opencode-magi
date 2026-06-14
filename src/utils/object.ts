3a23e426292574e21441ab4001db47c3a0c3065a
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2026-06-14 11:12:52 +0900

    feat(utils): add filterObject and omitNullish functions for object manipulation

--- src/utils/object.ts
+++ src/utils/object.ts
@@ -1,3 +1,4 @@
+import type { Dict } from "./index.type"
 import { isObject } from "./assertion"
 
 export function merge<T extends object>(base: any, override: any): T {
@@ -11,4 +12,23 @@ export function merge<T extends object>(base: any, override: any): T {
   }
 
   return merged
+}
+
+export function filterObject<T extends Dict, K extends Dict>(
+  obj: T,
+  func: (key: keyof T, value: T[keyof T], obj: T) => boolean,
+): K {
+  const result: Dict = {}
+
+  Object.entries(obj).forEach(([key, value]) => {
+    if (func(key, value, obj)) result[key] = value
+  })
+
+  return result as K
+}
+
+export function omitNullish<T extends Dict>(
+  obj: T,
+): Required<{ [K in keyof T]: Exclude<T[K], null | undefined> }> {
+  return filterObject(obj, (_, val) => val != null)
 }
